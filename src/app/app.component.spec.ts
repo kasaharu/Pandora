c89d10f2bef9705cddaa02e584162c38dfe80ad7
Wataru.Kasahara <kasahara1019@gmail.com>
2018-02-25 17:58:06 +0900

    [Test] AppComponent 内で TimerComponent を import

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        TimerComponent
       ],
     }).compileComponents();
   }));
