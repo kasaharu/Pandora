391abe7daf4c27999fd31ee40d0ffb7f8bacaa63
Wataru.Kasahara <kasahara1019@gmail.com>
2018-02-25 15:41:45 +0900

    [CLI Command] Generate timer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule
